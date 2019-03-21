4fc198526d61bb48473dffdbdc8f9c6571ff3608
simleo <simleo@crs4.it>
2019-03-21 14:46:25 +0100

    allow symlinks and directories as custom conf items

--- base/entrypoint.sh
+++ base/entrypoint.sh
@@ -13,7 +13,10 @@ nn_host=${NAMENODE_HOSTNAME:-${HOSTNAME}}
 sed -i s/localhost/${nn_host}/ "${conf_dir}"/core-site.xml
 
 if [ -d "${HADOOP_CUSTOM_CONF_DIR:-}" ]; then
-    find "${HADOOP_CUSTOM_CONF_DIR}" -maxdepth 1 -type f -exec \
+    if [ -e "${HADOOP_CUSTOM_CONF_DIR}/shellprofile.d" ]; then
+	rm -rf "${conf_dir}/shellprofile.d"
+    fi
+    find "${HADOOP_CUSTOM_CONF_DIR}" -mindepth 1 -maxdepth 1 -exec \
       ln -sfn {} "${conf_dir}"/ \;
 fi
 
---
+++ test/check_custom_conf.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+die() {
+    echo $1 1>&2
+    exit 1
+}
+
+img=crs4/hadoop-base:${HADOOP_VERSION}-${OS}
+new_bs=$((16*2**20))
+foo_content="export FOO=foo"
+
+wd=$(mktemp -d)
+mkdir -p "${wd}"/{data,hadoop}
+cp base/resources/conf/hadoop3/hdfs-site.xml "${wd}"/data/
+docker run --rm ${img} cat /opt/hadoop/etc/hadoop/hdfs-site.xml >"${wd}"/data/hdfs-site.xml
+sed -i "s|</configuration>|<property><name>dfs.blocksize</name><value>${new_bs}</value></property></configuration>|" "${wd}"/data/hdfs-site.xml
+ln -s ../data/hdfs-site.xml "${wd}"/hadoop/
+mkdir "${wd}"/hadoop/shellprofile.d
+echo "${foo_content}" >"${wd}"/hadoop/shellprofile.d/foo.sh
+bs=$(docker run --rm -v "${wd}":"${wd}":ro -e HADOOP_CUSTOM_CONF_DIR="${wd}"/hadoop ${img} hdfs getconf -confKey dfs.blocksize)
+[ ${bs} -eq ${new_bs} ] || die "failed to set custom blocksize"
+content=$(docker run --rm -v "${wd}":"${wd}":ro -e HADOOP_CUSTOM_CONF_DIR="${wd}"/hadoop ${img} cat /opt/hadoop/etc/hadoop/shellprofile.d/foo.sh)
+[ "${content}" == "${foo_content}" ] || die "failed to customize profile"
+rm -rf "${wd}"
