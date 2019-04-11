c777263e89412ccf2975b07e66b798660880732b
simleo <simleo@crs4.it>
2019-04-11 17:23:22 +0200

    add wget and simplify base image build

--- base/resources/install_hadoop.sh
+++ base/resources/install_hadoop.sh
@@ -4,10 +4,13 @@ set -euo pipefail
 
 this="${BASH_SOURCE-$0}"
 this_dir=$(cd -P -- "$(dirname -- "${this}")" && pwd -P)
-tar="${this_dir}/hadoop-${hadoop_version}.tar.gz"
+repo="http://www-eu.apache.org/dist/hadoop/common"
+tar="hadoop-${hadoop_version}.tar.gz"
 
 mkdir -p "${hadoop_home}"
-tar xf "${tar}" --strip 1 -C "${hadoop_home}"
+echo "getting ${repo}/hadoop-${hadoop_version}/${tar}"
+wget -q -O - "${repo}/hadoop-${hadoop_version}/${tar}" | \
+    tar xz --strip 1 -C "${hadoop_home}"
 major_version=${hadoop_version%%.*}
 from_conf="${this_dir}/conf/hadoop${major_version}"
 to_conf="${hadoop_home}/etc/hadoop"
@@ -17,8 +20,10 @@ for name in hadoop mapred yarn; do
 done
 
 if [ -n "${native_libs_dir:-}" ]; then
+    echo "copying native libs from ${native_libs_dir}"
     rm -rf "${hadoop_home}"/lib/native/*
     mv "${native_libs_dir}"/* "${hadoop_home}"/lib/native/
+    rm -rf "${native_libs_dir}"
 fi
 
 rm -rf "${hadoop_home}"/share/doc
